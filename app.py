8d3eb6d2dfa1657b2543edde1772e6de353f8580
teandra-davis <davisteandra@gmail.com>
2023-04-25 19:29:42 -0400

    Added some files and folders and started some basic database implemnetation

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, abort, redirect, render_template, request
+from src.models import db
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+# TODO: DB connection
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+
+db.init_app(app)
 
 #Landing Page
 @app.get('/')
@@ -15,6 +30,5 @@ def posts_questions():
     description = request.form['description']
     information = functional.create_post(title, browse, description)
     return redirect('/index')
-    
     
    
