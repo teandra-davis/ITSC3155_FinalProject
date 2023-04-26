c22b42a0eaaaa01c8196f13238d05c147e137f7e
teandra-davis <davisteandra@gmail.com>
2023-04-26 14:19:14 -0400

    Added some database implementation to app.py

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from src.models import db
 from dotenv import load_dotenv
 import os
 
+#TO DOWNLOAD ALL DEPENDENCIES, PIP INSTALL -R REQUIREMENTS.TXT
 load_dotenv()
 app = Flask(__name__)
 
+#LOCATED IN ENV.SAMPLE AND FILL OUT THE INFORMATION
+#postgres
 db_user = os.getenv('DB_USER')
+
 db_pass = os.getenv('DB_PASS')
+#postgres
 db_host = os.getenv('DB_HOST')
+#5432
 db_port = os.getenv('DB_PORT')
+#finalproject
 db_name = os.getenv('DB_NAME')
 
 # TODO: DB connection
