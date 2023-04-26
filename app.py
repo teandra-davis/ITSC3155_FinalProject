bfd7802443451a64bdc5318459bdb944813ff901
Saina <smacha1@gmail.com>
2023-04-26 14:04:41 -0400

    update

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, abort, redirect, render_template, request
 
 app = Flask(__name__)
 
+#Landing Page
 @app.get('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about', methods=[''])
+def about():
+    return render_template("about.html")
