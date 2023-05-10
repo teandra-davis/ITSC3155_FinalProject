f69d2d3dc2b7ec72d2ae0472fabb41c67dbcdb1f
Jenna Constantino <97635991+JennaConstantino@users.noreply.github.com>
2023-05-09 23:34:19 -0400

    I think i did this correctly

--- schema.sql
+++ schema.sql
@@ -3,25 +3,31 @@ CREATE TABLE IF NOT EXISTS user (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    date_added VARCHAR(255) NOT NULL,
+    date_added DATETIME NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS post (
-    book_id SERIAL,
-    title   VARCHAR(255) NOT NULL,
-    author  VARCHAR(255) NOT NULL,
-    rating  INT          NOT NULL,
-    PRIMARY KEY (book_id)
+    post_author SERIAL,
+    post_date   DATETIME NOT NULL,
+    post_time  VARCHAR(255) NOT NULL,
+    post_content  VARCHAR(255) NOT NULL,
+    post_comments VARCHAR(255) NOT NULL,
+    post_likes SERIAL,
+    post_subject VARCHAR(255) NOT NULL,
+    post_id SERIAL,
+    PRIMARY KEY (post_id)
 );
 
 CREATE TABLE IF NOT EXISTS comment (
-    book_id SERIAL,
-    title   VARCHAR(255) NOT NULL,
-    author  VARCHAR(255) NOT NULL,
-    rating  INT          NOT NULL,
-    PRIMARY KEY (book_id)
+    comment_author VARCHAR(255) NOT NULL,,
+    comment_date   DATETIME NOT NULL,
+    comment_content  VARCHAR(255) NOT NULL,
+    comment_likes  SERIAL,
+    comment_postid SERIAL,
+    PRIMARY KEY (comment_postid)
 );
+
 
