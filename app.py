1bcc230fb5116439f9964250c35ec0ce6c00da5b
teandra-davis <97898317+teandra-davis@users.noreply.github.com>
2023-05-11 17:49:18 -0400

    Revert "Aboutpage"

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ db.init_app(app)
 def index():
     return render_template('index.html')
 
-@app.route('/about', methods=[''])
-def about():
-    return render_template("about.html")
-
 #Showing all posts
 @app.get('/post/all')
 def list_all_posts():
